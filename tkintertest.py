9f0710c5270106e7a4859908db33163f02a19a87
Jorrit Lion <jtl@ipp-hgw.mpg.de>
2020-05-08 12:51:49 +0200

    Seems to work now. Missing function

--- tkintertest.py
+++ tkintertest.py
@@ -5,19 +5,23 @@ import PIL.Image, PIL.ImageTk
 import numpy as np
 import os
 
-mywidth=400
-myheight=400
+
 
 class GameofLife(Tk.Frame):
     '''
     Dislay an image on Tkinter.Canvas and delete it on button click
     '''
-    def __init__(self, parent):
+    def __init__(self, parent,width,height,res):
         '''
         Inititialize the GUI with a button and a Canvas objects
         '''
         Tk.Frame.__init__(self, parent)
         self.parent=parent
+
+        self.width = width
+        self.height = height
+        self.mult = res
+
         self.init_pic()
 
     def init_pic(self):
@@ -26,45 +30,50 @@ class GameofLife(Tk.Frame):
         """
         self.parent.title("Our Game of Life")       
         
-        self.arr =np.random.randint(0,high=2, size=(40,40)).astype(bool)
+        self.arr =np.random.randint(0,high=2, size=(int(self.height/self.mult),int(self.width/self.mult))).astype(bool)
+
         self.parent.grid_rowconfigure(0,weight=0)
         self.parent.grid_columnconfigure(0,weight=0)
 
-        self.parent.grid_columnconfigure(4,weight=1)
+        self.parent.grid_columnconfigure(5,weight=1)
         self.parent.grid_columnconfigure(0,weight=1)
 
-
         # Create a button and append it  a callback method to clear the image          
-        self.mybotton = Tk.Button(self.parent, text = 'run', command = self.runthingy)
-        self.mybotton.grid(row = 1, column = 0, sticky = "e")
+        self.button_run = Tk.Button(self.parent, text = 'Run', command = self.runthingy)
+        self.button_run.grid(row = 1, column = 0, sticky = "e")
 
         # Create another button         
-        self.mybotton2 = Tk.Button(self.parent, text = 'reset', command = self.reset)
-        self.mybotton2.grid(row = 1, column =1)
+        self.button_reset = Tk.Button(self.parent, text = 'Reset', command = self.reset)
+        self.button_reset.grid(row = 1, column =1)
 
-        self.button_random = Tk.Button(self.parent, text = 'randomize', command = self.randomize)
+        self.button_random = Tk.Button(self.parent, text = 'Randomize', command = self.randomize)
         self.button_random.grid(row = 1, column =2)
 
-        self.button_random = Tk.Button(self.parent, text = 'save', command = self.save)
-        self.button_random.grid(row = 1, column =3)
+        self.button_save = Tk.Button(self.parent, text = 'Save', command = self.save)
+        self.button_save.grid(row = 1, column =3)
 
-        self.button_random = Tk.Button(self.parent, text = 'open', command = self.open)
-        self.button_random.grid(row = 1, column =4,sticky="w")
+        self.button_open = Tk.Button(self.parent, text = 'Open', command = self.open)
+        self.button_open.grid(row = 1, column =4,sticky="w")
 
-        self.canvas = Tk.Canvas(self.parent, width = mywidth, height = myheight)
-        self.canvas.grid(row = 0,columnspan = 5)
+        self.button_adjust = Tk.Button(self.parent, text = 'Adjust', command = self.changesize)
+        self.button_adjust.grid(row = 1, column =5,sticky="w")
+
+        self.canvas = Tk.Canvas(self.parent, width = self.width, height = self.height)
+        self.canvas.grid(row = 0,columnspan = 6)
 
         self.canvas.bind("<Button-1>", self.callback)
 
         self.update_img()
+
+        
+
     
 
     def callback(self,event):
         '''
-        Callback method to delete image
+        Callback method to handle clicks
         '''
         self.parent.focus_set()
-        print("clicked at", event.x, event.y)
 
         self.adjust_image(event.x,event.y)
 
@@ -73,30 +82,25 @@ class GameofLife(Tk.Frame):
         '''
         Adjust the image to true/not true
         '''
-        pixelx = int(y/10)
-        pixely = int(x/10)
-
-        print("Trying to change pixel", pixelx,pixely)
+        pixelx = int(y/self.mult)
+        pixely = int(x/self.mult)
 
         self.arr[pixelx,pixely] = not(self.arr[pixelx,pixely])
 
 
         self.update_img()
-        #self.myphoto = PIL.Image.fromarray(self.arr).resize((mywidth,myheight),0)
-        #self.photo = PIL.ImageTk.PhotoImage(image = self.myphoto)
 
-        #self.canvas.create_image(0, myheight, image=self.photo, anchor=Tk.SW)
         
 
     @staticmethod
     def arr2photo(arr):
-        myphoto=PIL.Image.fromarray(arr).resize((mywidth,myheight),0)
+        myphoto=PIL.Image.fromarray(arr).resize((self.width,self.height),0)
         return PIL.ImageTk.PhotoImage(image = myphoto)
     
     def update_img(self):
-        self.myphoto = PIL.Image.fromarray(self.arr).resize((mywidth,myheight),0)
+        self.myphoto = PIL.Image.fromarray(self.arr).resize((self.width,self.height),0)
         self.photo = PIL.ImageTk.PhotoImage(image = self.myphoto)
-        self.canvas.create_image(0, myheight, image=self.photo, anchor=Tk.SW)
+        self.canvas.create_image(0, self.height, image=self.photo, anchor=Tk.SW)
 
 
     def runthingy(self):
@@ -105,17 +109,18 @@ class GameofLife(Tk.Frame):
     def reset(self):
         self.arr = np.zeros(self.arr.shape).astype(bool)
 
-        self.myphoto = PIL.Image.fromarray(self.arr).resize((mywidth,myheight),0)
-        self.photo = PIL.ImageTk.PhotoImage(image = self.myphoto)
-
-        self.canvas.create_image(0, 400, image=self.photo, anchor=Tk.SW)
+        self.update_img()
 
     def randomize(self):
-        pass
+        self.arr =np.random.randint(0,high=2, size=(int(self.height/self.mult),int(self.width/self.mult))).astype(bool)
+        self.update_img()
 
     def open(self):
         filename = Tk.filedialog.askopenfilename(initialdir=os.getcwd(),filetypes=[("Text Files", "*.dat")])
         loadedarr = np.loadtxt(filename)
+
+        self.arr = loadedarr.astype(bool)
+        self.update_img()
 
         
 
@@ -128,45 +133,25 @@ class GameofLife(Tk.Frame):
 
         np.savetxt(filename,self.arr)
 
+    def changesize(self):
+        width = Tk.simpledialog.askinteger("Input","Width",initialvalue=400)
+        height = Tk.simpledialog.askinteger("Input","Height",initialvalue=400)
+        res = Tk.simpledialog.askinteger("Input","Resolution",initialvalue=10)
 
-"""
-def callback(event):
-    window.focus_set()
-    print("clicked at", event.x, event.y)
+        if not width or not height or not res:
+            return
+            
 
-"""
-"""
-window = tkinter.Tk()
-
-window.bind("<Button-1>", callback)
-
-window.title("Our Game of Life")
-
-
-
-
-height, width = cv_img.shape
-
-canvas = tkinter.Canvas(window, width = mywidth, height = myheight)
-canvas.pack()
-
-myphoto=PIL.Image.fromarray(cv_img).resize((mywidth,myheight),0)
-
-
-photo = PIL.ImageTk.PhotoImage(image = myphoto)
-
-
-
-canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
-
-window.mainloop()
-"""
-
+        self.width = width
+        self.height = height
+        self.mult = res
+       
+        self.init_pic()
 
 # Main method
 def main():
     root=Tk.Tk()
-    d=GameofLife(root)
+    d=GameofLife(root,600,400,10)
     root.mainloop()
 
 # Main program       
