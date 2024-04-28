6a7b05a899da859276afc7b2c5dd56e486994ba2
lionjo <41913876+lionjo@users.noreply.github.com>
2024-04-28 11:58:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ setup(
     name="GameOfLife",
     version="0.1",
     description="A codeframwork play around with Convey's 'Game of Life'",
-    # url="https://gitlab.mpcdf.mpg.de/jtl/equi-surf",
     author="Jorrit Lion",
     author_email="jorrit.lion@gmail.com",
     license="MIT",
     packages=find_packages() + find_namespace_packages(),
     install_requires=["numpy", "Pillow", "scipy"],
     scripts=["scripts/OurGameOfLife.py"],
-    python_requires=">=3.6.*",
     zip_safe=False,
 )
